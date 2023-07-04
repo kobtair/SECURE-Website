a23171ae36d534806e6afcfa9fb0fd9587508e09
kobtair <aaqibimran0@gmail.com>
2023-07-04 10:23:53 +0500

    new commit

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -23,8 +23,8 @@ const Blogs = () => {
     <div className=" animate__animated animate__fadeIn blogs">
       <h1 className='text-center blogs-title'>Blogs</h1>
       {blogs.map(blog => (
-        <div className="blog" key={blog._id}>
-          <h2 className='whitespace-normal'>{blog.title}</h2>
+        <div className="blog cursor-pointer "  key={blog._id}>
+          <h2 className='whitespace-normal hover:underline'>{blog.title}</h2>
           <p className="blog-meta">{blog.publishedAt} | By {blog.author}</p>
           <p className="blog-content">{blog.content}</p>
         </div>
